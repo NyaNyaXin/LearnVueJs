4d17d58e9bfa299ff613963df4fd06e54dd62c82
NyaNyaXin <chenxin95013>
2017-11-09 10:15:18 +0800

    Vue-router入门

---
+++ VueCLI/vueclitest/src/components/Hi.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>{{message}}</h2>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'Hi',
+    data () {
+      return {
+        message: 'Im Hi page'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
