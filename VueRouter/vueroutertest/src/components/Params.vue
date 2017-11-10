53cc856162a4b68631b2d1ba3b43a760bb5610eb
NyaNyaXin <chenxin95013>
2017-11-10 14:11:45 +0800

    Vue-router路由中的钩子函数

--- VueRouter/vueroutertest/src/components/Params.vue
+++ VueRouter/vueroutertest/src/components/Params.vue
@@ -11,6 +11,14 @@
       return {
         msg: 'Params page'
       }
+    },
+    beforeRouteEnter: (to, from, next) => {
+      console.log('准备进入params路由模板')
+      next()
+    },
+    beforeRouteLeave: (to, from, next) => {
+      console.log('准备离开params路由模板')
+      next()
     }
   }
 </script>
